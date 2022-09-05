c601978a2780bf4efbd05e191db2542ddf25bd21
Nico De Witte <nico.dewitte@outlook.com>
2022-09-05 19:00:35 +0200

    Update docker compose and setup auth for mongodb

--- backend/src/mongoose.ts
+++ backend/src/mongoose.ts
@@ -5,9 +5,14 @@ import logger from './logger';
 export default function (app: Application): void {
   const config = app.get('mongodb');    // Get config object by key
   const connection = `mongodb://${config.host}:${config.port}/${config.database}`;
-
-  mongoose.connect(connection)
+  
+  mongoose.connect(connection, {
+    authSource: "admin",
+    user: config.root_username,
+    pass: config.root_password
+  })
   .catch(err => {
+    console.log("Failed to connect to mongodb")
     logger.error(err);
     process.exit(1);
   });
