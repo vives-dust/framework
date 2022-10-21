924dbd5785e64c1e024105ac18dcfb09da284b54
Nico De Witte <nico.dewitte@outlook.com>
2022-10-21 16:16:25 +0200

    Create sensors service

--- backend/src/services/index.ts
+++ backend/src/services/index.ts
@@ -3,6 +3,7 @@ import users from './users/users.service';
 import devices from './devices/devices.service';
 import soilmodels from './soilmodels/soilmodels.service';
 import measurements from './measurements/measurements.service';
+import sensors from './sensors/sensors.service';
 // Don't remove this comment. It's needed to format import lines nicely.
 
 export default function (app: Application): void {
@@ -10,4 +11,5 @@ export default function (app: Application): void {
   app.configure(devices);
   app.configure(soilmodels);
   app.configure(measurements);
+  app.configure(sensors);
 }
