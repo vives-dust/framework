3bba679f2ffc72d0061ff81105cf6ef206006e65
Noah Debaere <noah.debaere@gmail.com>
2022-12-02 09:39:02 +0100

    added conversionmodel interfaces

--- backend/src/services/conversionmodels/conversionmodels.class.ts
+++ backend/src/services/conversionmodels/conversionmodels.class.ts
@@ -1,6 +1,22 @@
 import { Service, MongooseServiceOptions } from 'feathers-mongoose';
 import { Application } from '../../declarations';
 
+export interface ConversionModelSample {
+  raw: number;
+  value: number;
+}
+
+export interface ConversionModel {
+  _id?: string;
+  name: string;
+  description: string;
+  input_unit: string;
+  output_unit: string;
+  samples: {
+    type: [ConversionModelSample]
+  };
+}
+
 export class Conversionmodels extends Service {
   //eslint-disable-next-line @typescript-eslint/no-unused-vars
   constructor(options: Partial<MongooseServiceOptions>, app: Application) {
