378d2fcd01ac9392699c10de7888d9a65b5a8573
Noah Debaere <noah.debaere@gmail.com>
2022-12-02 18:01:56 +0100

    input validation for devices

---
+++ backend/src/validation/device.ts
@@ -0,0 +1,21 @@
+import Joi from 'joi';
+import { ImageSchema } from './partials/image';
+import { LocationSchema } from './partials/location';
+import { NanoidSchema } from './partials/nanoid';
+import { ObjectIdSchema } from './partials/objectid';
+
+const DeviceCreateSchema = Joi.object().keys({
+    _id: ObjectIdSchema,
+    id: NanoidSchema.required(),
+    name: Joi.string().required(),
+    description: Joi.string().required(),
+    tree_id: NanoidSchema.required(),       // ToDo: does this need to be mongo ObjectId or nanoId?
+    devicetype_id: ObjectIdSchema.required(),
+    datasource_key: Joi.string().required()
+  })
+  
+export const DeviceSchemas = {
+
+    _create: DeviceCreateSchema,
+
+  };
