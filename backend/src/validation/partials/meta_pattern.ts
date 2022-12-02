0a721b2375636dd33f256c3ac3a14bcc12d01cd6
Nico De Witte <nico.dewitte@outlook.com>
2022-12-02 17:40:37 +0100

    Fix conversion model id to object id

--- backend/src/validation/partials/meta_pattern.ts
+++ backend/src/validation/partials/meta_pattern.ts
@@ -1,9 +1,9 @@
 import Joi from 'joi';
-import { NanoidSchema } from './nanoid';
+import { ObjectIdSchema } from './objectid';
 
 const MetaPatternSchema = Joi.object().keys({
     depth: Joi.number().less(0).negative(),
-    conversion_model_id: NanoidSchema
+    conversion_model_id: ObjectIdSchema,
 }).allow({})
 
 export { MetaPatternSchema }
