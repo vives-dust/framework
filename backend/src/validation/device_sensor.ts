8f1483d1587d261bb9a60b98d72ac7e302ecce2a
Nico De Witte <nico.dewitte@outlook.com>
2022-12-03 10:57:44 +0100

    Fix difference between tags and tag-specs for sensor-type and actual sensor

--- backend/src/validation/device_sensor.ts
+++ backend/src/validation/device_sensor.ts
@@ -1,7 +1,7 @@
 import Joi from 'joi';
 
 import { MongoObjectIdSchema } from './partials/mongo_object_id';
-import { DataSourceSchema } from './partials/datasource';
+import { DataSourceSpecSchema } from './partials/datasource';
 import { MetaSchema } from './partials/meta';
 
 // Management Schema's
@@ -10,7 +10,7 @@ const DeviceSensorCreateSchema = Joi.object().keys({
   _id: MongoObjectIdSchema,
   devicetype_id: MongoObjectIdSchema.required(),
   sensortype_id: MongoObjectIdSchema.required(),
-  data_source: DataSourceSchema.required(),
+  data_source: DataSourceSpecSchema.required(),
   meta: MetaSchema.required(),
 });
 
--- backend/src/validation/partials/datasource.ts
+++ backend/src/validation/partials/datasource.ts
@@ -1,16 +1,21 @@
 import Joi from 'joi';
+import { TagsSchema, TagsSpecSchema } from './tags';
 
-// TODO: DataSource is very strict for the moment
-const DataSourceSchema = Joi.object().keys({
+// TODO: DataSource is very strict for the moment and will only work with the current known data sources (influx)
+
+// Spec is for SensorType where we define the binding between our models
+// and the data source representation. Example devId => datasource_key
+const DataSourceSpecSchema = Joi.object().keys({
   source: Joi.string().valid('influxdb').required(),
   bucket: Joi.string().valid('dust').required(),
   measurement: Joi.string().valid('dust-sensor', 'weatherstation', 'weatherapi').required(),
-  tags: Joi.object().keys({
-    // TODO: Again, very strict for the moment.
-    devId: Joi.string().valid('datasource_key'),
-    stationId: Joi.string().valid('datasource_key'),
-  }).min(1).required(),
+  tags: TagsSpecSchema.required(),
   field: Joi.string().required(),
 });
 
-export { DataSourceSchema };
+// Normal is for Sensor where the key fields are filled with the actual data source value.
+const DataSourceSchema = DataSourceSpecSchema.keys({
+  tags: TagsSchema.required(),      // We override the specs here
+});
+
+export { DataSourceSpecSchema, DataSourceSchema };
