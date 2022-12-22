92cabbc9d5507d207d5306adc6308cdfc0502a8f
Noah Debaere <noah.debaere@gmail.com>
2022-12-22 14:46:35 +0100

    conversionmodel middleware to sanitize output after creation and changed validation schemes

--- backend/src/validation/conversionmodel.ts
+++ backend/src/validation/conversionmodel.ts
@@ -6,18 +6,22 @@ import { UnitSchema } from './partials/unit';
 
 // Management Schema's
 
-const ConversionModelCreateSchema = Joi.object().keys({
-  _id: MongoObjectIdSchema,
+const ConversionModelBaseSchema = Joi.object().keys({
   id: NanoIdSchema.required(),
   name: Joi.string().required(),
   description: Joi.string().required(),
   input_unit: UnitSchema.required(),
   output_unit: UnitSchema.required(),
+})
+
+const ConversionModelCreateSchema = ConversionModelBaseSchema.keys({
   samples: Joi.array().min(1).items(ConversionSampleSchema).required(),
 });
+
+
   
 export const ConversionModelSchemas = {
 
   _create: ConversionModelCreateSchema,
-
+  _sanitize: ConversionModelBaseSchema
 };
