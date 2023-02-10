bf039d73f1335ce0a5bac89b338cdbdcc74a0cee
Noah Debaere <noah.debaere@gmail.com>
2023-02-10 18:06:03 +0100

    rewrote test for users, db integration not working yet

--- backend/test/services/users.test.ts
+++ backend/test/services/users.test.ts
@@ -7,4 +7,39 @@ describe('\'users\' service', () => {
 
     assert.ok(service, 'Registered the service');
   });
+
+  it('create a user and encrypts password', async () => {
+    // Requests for creating users will always be external
+    const params = { provider: 'external' };
+
+    const user = await app.service('users').create({
+      email: 'chuck@norris.com',
+      password: 'verysecret',
+      name: 'Chuck Norris'
+    }, 
+    params
+    );
+    
+    // Makes sure the password got encrypted
+    assert.ok(user.password !== 'verysecret');
+  });
+
+  it('removes password after creation', async () => {
+    // Setting `provider` indicates an external request
+    const params = { provider: 'rest' };
+
+    const user = await app.service('users').create(
+      {
+        email: 'test@example.com',
+        password: 'verysecret'
+      },
+      params
+    );
+
+    // Makes sure the password got encrypted
+    assert.ok(user.password !== 'verysecret');
+
+    // Make sure password has been removed
+    assert.ok(!user.password);
+  });
 });
