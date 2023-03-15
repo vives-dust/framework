b902c8ae0ac6aedbc9a492762cb189351dd271c7
Nico De Witte <bioboost@gmail.com>
2023-03-15 21:29:06 +0100

    Move app hooks to separate file

--- dust-backend/src/app.ts
+++ dust-backend/src/app.ts
@@ -5,15 +5,12 @@ import { koa, rest, bodyParser, errorHandler, parseAuthentication, cors, serveSt
 import socketio from '@feathersjs/socketio'
 
 import { configurationValidator } from './configuration'
-import type { Application, HookContext } from './declarations'
-import { logError } from './hooks/log-error'
+import type { Application } from './declarations'
 import { mongodb } from './mongodb'
 import { authentication } from './authentication'
 import { services } from './services/index'
 import { channels } from './channels'
-import { authenticate } from '@feathersjs/authentication'
-import { require_admin } from './hooks/authorization'
-import { disallow, iff } from 'feathers-hooks-common'
+import { hooks, setupTeardownHooks } from './app.hooks'
 
 const app: Application = koa(feathers())
 
@@ -42,41 +39,8 @@ app.configure(authentication)
 app.configure(services)
 
 // Register hooks that run on all service methods
-app.hooks({
-  around: {
-    all: [logError]
-  },
-  before: {
-    create: [
-      iff(       // If not authenticating or registering user, require admin
-        (context : HookContext) => !(
-          context.path === 'authentication'      // Get auth token
-          || context.path === 'users'            // Register user
-        ),
-        ...[
-          authenticate('jwt'),
-          require_admin,
-        ]
-      ),
-    ],
-    update: [ disallow('external') ],
-    patch: [
-      iff(  // For the moment we only allow user details to be patched
-        context => (context.path === 'users'),
-        authenticate('jwt'),
-      ).else(
-        disallow('external')
-      ),
-    ],
-    remove: [ disallow('external') ]
-  },
-  after: {},
-  error: {}
-})
+app.hooks(hooks)
 // Register application setup and teardown hooks here
-app.hooks({
-  setup: [],
-  teardown: []
-})
+app.hooks(setupTeardownHooks)
 
 export { app }
